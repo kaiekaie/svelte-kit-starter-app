6990391433a24dafee9e25d822f5ce66e0bffc07
Magnus Aga <magnusaga@gmail.com>
2022-05-12 15:34:40 +0200

    added animation

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,19 @@
+<script context="module" lang="ts">
+	export const load = async (loadInput: LoadInput) => ({
+		props: {
+			key: loadInput.url.pathname
+		}
+	});
+</script>
+
 <script lang="ts">
 	import '../app.postcss';
 	import json from '$/lang/en.json';
+	import Animate from '$lib/components/Animate.svelte';
+	import type { Load, LoadInput } from '@sveltejs/kit';
 	let main: HTMLElement | null = null;
+
+	export let key: string;
 </script>
 
 <svelte:head>
@@ -9,7 +21,14 @@
 		{json.title}
 	</title>
 </svelte:head>
-
+<header>
+	<nav>
+		<a href="/">Home</a>
+		<a href="/about">About</a>
+	</nav>
+</header>
 <div bind:this={main} class="container dark:bg-gray-900 bg-white m-auto px-4 h-screen py-8 ">
-	<slot />
+	<Animate refresh={key}>
+		<slot />
+	</Animate>
 </div>
